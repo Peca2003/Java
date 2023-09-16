0b2150eb82a0abb63138e9501735535e3a08cd52
Peca2003 <90759048+Peca2003@users.noreply.github.com>
2023-09-16 10:54:50 +0300

    Update Laba 6, ex. 2.java

--- Laba 6, ex. 2.java
+++ Laba 6, ex. 2.java
@@ -9,9 +9,9 @@ public class Main
 		matrixD = new int[3][3];
 		matrixE = new int[3][3];
 		System.out.println("Матрица C: ");
-		for (int i = 0; i < 3; i++)
+		for (int i = 0; i < array.length; i++)
 		{
-		    for (int j = 0; j < 3; j++)
+		    for (int j = 0; j < array[i].length; j++)
 		    {
 		        matrixC[i][j] = (int)Math.round(Math.random() * 99);
 		        System.out.print(matrixC[i][j] + " ");
@@ -19,9 +19,9 @@ public class Main
 		    System.out.println();
 		}
 		System.out.println("\nМатрица D: ");
-		for (int i = 0; i < 3; i++)
+		for (int i = 0; i < array.length; i++)
 		{
-		    for (int j = 0; j < 3; j++)
+		    for (int j = 0; j < array[i].length; j++)
 		    {
 		        matrixD[i][j] = (int)Math.round(Math.random() * 99);
 		        System.out.print(matrixD[i][j] + " ");
@@ -31,9 +31,9 @@ public class Main
 		System.out.println("\nСумма матриц: ");
 		int [][] sumMatrix;
 		sumMatrix = new int[3][3];
-		for (int i = 0; i < 3; i++)
+		for (int i = 0; i < array.length; i++)
 		{
-		    for (int j = 0; j < 3; j++)
+		    for (int j = 0; j < array[i].length; j++)
 		    {
 		        sumMatrix[i][j] = matrixC[i][j] + matrixD[i][j];
 		        System.out.print(sumMatrix[i][j] + " ");
