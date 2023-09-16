dac38ebf6d486d0d1b241ff12bc31ddb4e0656fc
Peca2003 <90759048+Peca2003@users.noreply.github.com>
2023-09-16 10:56:13 +0300

    Update Laba 6, ex. 1.java

--- Laba 6, ex. 1.java
+++ Laba 6, ex. 1.java
@@ -4,9 +4,9 @@ public class Main
 	{
 		int [][] matrix;
 		matrix = new int[5][6];
-		for (int i = 0; i < 5; i++)
+		for (int i = 0; i < array.length; i++)
 		{
-		    for (int j = 0; j < 6; j++)
+		    for (int j = 0; j < array[i].length; j++)
 		    {
 		        matrix[i][j] = (int)Math.round(Math.random() * 99);
 		        System.out.print(matrix[i][j] + " ");
@@ -14,9 +14,9 @@ public class Main
 		    System.out.println();
 		}
 		System.out.println("\nТретья строка матрицы: ");
-		for (int i = 2; i < 3; i++)
+		for (int i = 2; i < array.length; i++)
 		{
-		    for (int j = 0; j < 6; j++)
+		    for (int j = 0; j < array[i].length; j++)
 		    {
 		        System.out.print(matrix[i][j] + " ");
 		    }
