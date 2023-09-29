23ec4a1f06279cf3c435f9f469ed0f73f6161e18
Peca2003 <90759048+Peca2003@users.noreply.github.com>
2023-09-29 12:30:21 +0300

    Update Triangle.java

--- src/ex/Triangle.java
+++ src/ex/Triangle.java
@@ -2,25 +2,23 @@ package ex;
 
 public class Triangle
 {
-	private double firstSide;
-	private double secondSide;
-	private double thirdSide;
-	
-	public Triangle(double firstSide, double secondSide, double thirdSide)
-	{
-		this.firstSide = firstSide;
-		this.secondSide = secondSide;
-		this.thirdSide = thirdSide;
-	}
-	
-	public double getFirstSide() { return firstSide; }
-	public void setFirstSide(double firstSide) { this.firstSide = firstSide; }
-	
-	public double getSecondSide() { return secondSide; }
-	public void setSecondSide(double secondSide) { this.secondSide = secondSide; }
-	
-	public double getThirdSide() { return thirdSide; }
-	public void setThirdSide(double thirdSide) { this.thirdSide = thirdSide; }
-	
-	
+    private double firstSide;
+    private double secondSide;
+    private double thirdSide;
+
+    public Triangle(double a, double b, double c)
+    {
+        this.firstSide = a;
+        this.secondSide = b;
+        this.thirdSide = c;
+    }
+
+    public double getFirstSide() { return firstSide; }
+    public void setFirstSide(double a) { this.firstSide = a; }
+
+    public double getSecondSide() { return secondSide; }
+    public void setSecondSide(double b) { this.secondSide = b; }
+
+    public double getThirdSide() { return thirdSide; }
+    public void setThirdSide(double c) { this.thirdSide = c; }
 }
