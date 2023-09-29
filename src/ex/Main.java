e6f0a77723b7412aa7841c9024b626b0d2a0c727
Peca2003 <90759048+Peca2003@users.noreply.github.com>
2023-09-29 12:30:58 +0300

    Update Main.java

--- src/ex/Main.java
+++ src/ex/Main.java
@@ -1,17 +1,17 @@
 package ex;
 
-public class Main {
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Rectangle rectangle = new Rectangle(4, 6);
+        Triangle triangle = new Triangle(5, 4, 6);
 
-	public static void main(String[] args)
-	{
-		Rectangle rectangle = new Rectangle(4, 6);
-		Triangle triangle = new Triangle(5, 4, 6);
-		
-		Calculations calculations = new Calculations();
-		calculations.areaСalculation(rectangle.getFirstSide(), rectangle.getSecondSide());
-		calculations.areaСalculation(triangle.getFirstSide(), triangle.getSecondSide(), triangle.getThirdSide());
-		
-		calculations.perimeterСalculation(rectangle.getFirstSide(), rectangle.getSecondSide());
-		calculations.perimeterСalculation(triangle.getFirstSide(), triangle.getSecondSide(), triangle.getThirdSide());
-	}
+        Calculations calculations = new Calculations();
+
+        System.out.println("Площадь прямоугольника = " + calculations.areaСalculation(rectangle.getFirstSide(), rectangle.getSecondSide()));
+        System.out.println("Площадь треугольника = " + calculations.areaСalculation(triangle.getFirstSide(), triangle.getSecondSide(), triangle.getThirdSide()));
+        System.out.println("\nПериметр прямоугольника = " + calculations.perimeterСalculation(rectangle.getFirstSide(), rectangle.getSecondSide()));
+        System.out.println("Периметр треугольника = " + calculations.perimeterСalculation(triangle.getFirstSide(), triangle.getSecondSide(), triangle.getThirdSide()));
+    }
 }
