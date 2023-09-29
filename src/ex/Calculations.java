671114428cf5a32719580e6baa55daf9b81903b5
Peca2003 <90759048+Peca2003@users.noreply.github.com>
2023-09-29 12:29:56 +0300

    Update Calculations.java

--- src/ex/Calculations.java
+++ src/ex/Calculations.java
@@ -2,24 +2,24 @@ package ex;
 
 public class Calculations
 {
-	public void areaСalculation(double side1, double side2)
-	{
-		System.out.println(side1 * side2);
-	}
-	
-	public void areaСalculation(double side1, double side2, double side3)
-	{
-		double p = 0.5 * (side1 + side2 + side3);
-		System.out.println(Math.sqrt(p * (p - side1) * (p - side2) * (p - side3)));
-	}
-	
-	public void perimeterСalculation(double side1, double side2)
-	{
-		System.out.println(2 * (side1 + side2));
-	}
-	
-	public void perimeterСalculation(double side1, double side2, double side3)
-	{
-		System.out.println(side1 + side2 + side3);
-	}
+    public double areaСalculation(double side1, double side2)
+    {
+        return (side1 * side2);
+    }
+
+    public double areaСalculation(double side1, double side2, double side3)
+    {
+        double p = 0.5 * (side1 + side2 + side3);
+        return (Math.sqrt(p * (p - side1) * (p - side2) * (p - side3)));
+    }
+
+    public double perimeterСalculation(double side1, double side2)
+    {
+        return (2 * (side1 + side2));
+    }
+
+    public double perimeterСalculation(double side1, double side2, double side3)
+    {
+        return (side1 + side2 + side3);
+    }
 }
