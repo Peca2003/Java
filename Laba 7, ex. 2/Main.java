ecea261174b7c97cd0e0a0588a613839d5cd71d7
Peca2003 <90759048+Peca2003@users.noreply.github.com>
2023-11-11 12:56:53 +0300

    Create Main.java

---
+++ Laba 7, ex. 2/Main.java
@@ -0,0 +1,16 @@
+package mountain;
+import java.util.Scanner;
+
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in, "utf-8");
+        System.setProperty("console.encoding","utf-8");
+        int numberMountains = sc.nextInt();
+        Mountain mount;
+        Mountain [] mountain = new Mountain[numberMountains];
+    }
+}
