862a7cefc5334e9082e78556b220fd0d31b16201
Peca2003 <90759048+Peca2003@users.noreply.github.com>
2023-11-11 12:21:04 +0300

    Create Mountain.java

---
+++ Laba 7, ex. 1/Mountain.java
@@ -0,0 +1,37 @@
+package mountain;
+
+public class Mountain
+{
+    private String m_MountainName, m_Location;
+    private int m_MountainHeight;
+
+
+    public String getM_MountainName() {
+        return m_MountainName;
+    }
+
+
+    public void setM_MountainName(String m_MountainName) {
+        this.m_MountainName = m_MountainName;
+    }
+
+
+    public String getM_Location() {
+        return m_Location;
+    }
+
+
+    public void setM_Location(String m_Location) {
+        this.m_Location = m_Location;
+    }
+
+
+    public int getM_MountainHeight() {
+        return m_MountainHeight;
+    }
+
+
+    public void setM_MountainHeight(int m_MountainHeight) {
+        this.m_MountainHeight = m_MountainHeight;
+    }
+}
