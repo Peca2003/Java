8081acad31f5457f4000927f122da2c3fb37f1b2
Peca2003 <90759048+Peca2003@users.noreply.github.com>
2023-11-14 13:27:11 +0300

    Update Main.java

--- Laba 7, ex. 2/Main.java
+++ Laba 7, ex. 2/Main.java
@@ -1,16 +1,25 @@
 package mountain;
+
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
+import static mountain.Mountain.*;
 
 public class Main
 {
-
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in, "utf-8");
+        Scanner sc = new Scanner(System.in, StandardCharsets.UTF_8);
         System.setProperty("console.encoding","utf-8");
-        int numberMountains = sc.nextInt();
-        Mountain mount;
-        Mountain [] mountain = new Mountain[numberMountains];
+
+        System.out.println("Введите количество вершин: ");
+        int countMountains = sc.nextInt();
+        Mountain [] mountains = setMountainArray(countMountains);
+
+        showMountainArray(mountains);
+        mountainMaxHeight(mountains);
+        mountainHeight(mountains);
+        sortMountainArray(mountains);
+        searchMountain(mountains);
     }
 }
