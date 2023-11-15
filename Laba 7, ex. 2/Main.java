16b8ba62a6a8ee82d7a3a117cc9adf604b6e8cb2
Peca2003 <90759048+Peca2003@users.noreply.github.com>
2023-11-15 15:56:31 +0300

    Update Main.java

--- Laba 7, ex. 2/Main.java
+++ Laba 7, ex. 2/Main.java
@@ -10,7 +10,6 @@ public class Main
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in, StandardCharsets.UTF_8);
-        System.setProperty("console.encoding","utf-8");
 
         System.out.println("Введите количество вершин: ");
         int countMountains = sc.nextInt();
