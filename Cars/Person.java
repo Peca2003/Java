655edaf8db7856f6c50031531f8ba9aa3ee9339b
Peca2003 <90759048+Peca2003@users.noreply.github.com>
2023-11-24 14:36:57 +0300

    Create Person.java

---
+++ Cars/Person.java
@@ -0,0 +1,14 @@
+package com.company.professions;
+
+public class Person
+{
+    private int m_Age;
+
+    public Person(int m_Age) {
+        this.m_Age = m_Age;
+    }
+
+    public int getM_Age() {
+        return m_Age;
+    }
+}
