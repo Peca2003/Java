9aa203770fb9864efd0f8b651b37d16b30d58fb0
Peca2003 <90759048+Peca2003@users.noreply.github.com>
2023-11-29 15:24:01 +0300

    Update Main.java

--- Cars/Main.java
+++ Cars/Main.java
@@ -2,7 +2,6 @@ package cars;
 
 import com.company.details.Engine;
 import com.company.professions.Driver;
-import com.company.professions.Person;
 import com.company.vehicles.Car;
 import com.company.vehicles.Lorry;
 import com.company.vehicles.SportCar;
@@ -11,13 +10,22 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Person driverLorry = new Driver(35, 27);
-        Person driverSport = new Driver(37, 12);
+        Driver driverLorry = new Driver(35, 27);
+        Driver driverSport = new Driver(37, 12);
         Engine engineLorry = new Engine(250, "Volvo");
         Engine engineSport = new Engine(1000, "Bugatti");
-        Car lorry = new Lorry("Грузовик", engineLorry, (Driver) driverLorry, "Volvo", 35);
-        Car sport = new SportCar("Спорткар", engineSport, (Driver) driverSport, "Bugatti", 320);
+        Car lorry = new Lorry("Грузовик", engineLorry, driverLorry, "Volvo", 35);
+        Car sport = new SportCar("Спорткар", engineSport, driverSport, "Bugatti", 320);
+
+        sport.start();
+        sport.turnLeft();
+        sport.start();
+        sport.turnRight();
+        sport.stop();
+        System.out.println("\n");
+
         System.out.println(lorry.toString());
+        System.out.println("\n");
         System.out.println(sport.toString());
     }
 }
