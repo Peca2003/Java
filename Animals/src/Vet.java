47c8511ffc43ef73b7cb4512a34cf911456acfc8
Peca2003 <90759048+Peca2003@users.noreply.github.com>
2023-11-29 17:09:25 +0300

    Delete Animals/src directory

--- Animals/src/Vet.java
+++
@@ -1,9 +0,0 @@
-public class Vet
-{
-    public void treatAnimal(Animal animal)
-    {
-        System.out.println("Имя: " + animal.getM_Name());
-        System.out.print("Описание: ");
-        animal.getDescription();
-    }
-}
