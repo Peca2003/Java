5eb1191bbf2f45a9a5a1dfbd1e83ddaeb53dfc05
Peca2003 <90759048+Peca2003@users.noreply.github.com>
2023-12-01 14:06:32 +0300

    Add files via upload

---
+++ Fruits/Apricot.java
@@ -0,0 +1,14 @@
+public class Apricot extends Fruits
+{
+    private double m_Price;
+
+    public Apricot(double m_Weight, double m_Price) {
+        super(m_Weight);
+        this.m_Price = m_Price;
+    }
+
+    @Override
+    public double priceFruit() {
+        return m_Weight * m_Price;
+    }
+}
